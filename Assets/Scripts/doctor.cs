91cc2f36c41c0c9a79069bbb947b0fbb932c1743
melike <melike1kir@gmail.com>
2025-11-05 22:16:11 +0300

    little adjustment

--- Assets/Scripts/doctor.cs
+++ Assets/Scripts/doctor.cs
@@ -16,16 +16,33 @@ public class doctor : MonoBehaviour
 
     private NavMeshAgent agent;
     private bool isChasing = false;
-
+    private HealthEnemy healthEnemy;
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
         agent.speed = moveSpeed;
-        agent.isStopped = true; // Başta duruyor
+        agent.isStopped = true;
+        healthEnemy = GetComponent<HealthEnemy>();
     }
 
     void Update()
     {
+        if (healthEnemy != null && healthEnemy.isDead)
+        {
+            if (agent.enabled)
+            {
+                agent.isStopped = true;
+                agent.enabled = false;
+            }
+            if (animator != null)
+            {
+                animator.SetBool(walkBoolName, false);
+                // Burada ölüm animasyonu tetikleyicisini de çağırabilirsiniz:
+               // animator.SetTrigger("Die"); 
+            }
+            isChasing = false; // Kovalamayı tamamen durdur
+            return; // Update'in geri kalanını çalıştırma
+        }
         if (isChasing && player != null)
         {
             agent.SetDestination(player.position);
